6fd74c954308a789e8b875b5f8d5ca148da48bd8
Megarcia-fiuba <megarcia@fi.uba.ar>
2018-09-11 11:59:45 -0300

    Added CapgeminiDB and DaoException.

---
+++ src/main/java/com/cagmeini/serviciosya/dao/CapgeminiDB.java
@@ -0,0 +1,38 @@
+package com.cagmeini.serviciosya.dao;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public abstract class CapgeminiDB {
+
+    private static final BasicDataSource dataSource= new BasicDataSource();
+
+    public CapgeminiDB() throws Exception{
+        try{
+
+            Properties pop = new Properties();
+            pop.load(CapgeminiDB.class.getClassLoader().
+                    getResourceAsStream("jdbc.properties"));
+
+            dataSource.setUrl(pop.getProperty("jdbc.url"));
+            dataSource.setUsername (pop.getProperty("jdbc.user"));
+            dataSource.setPassword (pop.getProperty("jdbc.pw"));
+            dataSource.setMinIdle (5);
+            dataSource.setMaxIdle (10);
+            dataSource.setMaxOpenPreparedStatements (100);
+
+        }catch(Exception e){
+            throw new DaoException(e);
+        }
+    }
+
+    protected Connection getConnection() throws SQLException {
+        return this.dataSource.getConnection();
+
+
+    }
+
+}
--- src/main/java/com/cagmeini/serviciosya/dao/OccupationJdbcDao.java
+++ src/main/java/com/cagmeini/serviciosya/dao/OccupationJdbcDao.java
@@ -2,33 +2,150 @@ package com.cagmeini.serviciosya.dao;
 
 import com.cagmeini.serviciosya.beans.domain.Occupation;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class OccupationJdbcDao implements IOccupationDao {
+public class OccupationJdbcDao extends CapgeminiDB implements IOccupationDao   {
+
+    private CapgeminiDB db;
+
+    public OccupationJdbcDao() throws Exception {
+    }
 
     @Override
-    public List<Occupation> findAllOccupations() {
-    return null;
-  }
+    public List<Occupation> findAllOccupations() throws Exception {
+        try {
+
+            Connection cnn= this.db.getConnection();
+
+            Statement stm = cnn.createStatement ();
+
+            // Result set get the result of the SQL query
+            ResultSet resultSet = stm.executeQuery ("SELECT * from \"OCCUPATION\"");
+
+            // Build the occupation list.
+            List<Occupation> list = new ArrayList<>();
+            while (resultSet.next()) {
+                int id= resultSet.getInt("ID");
+                String name= resultSet.getString("NAME");
+                String description = resultSet.getString("DESCRIPTION");
+
+                Occupation oc = new Occupation(String.valueOf(id),name,description);
+
+                list.add (oc);
+            }
+
+            // Return the occupations.
+            return list;
+
+        } catch (Exception e) {
+            throw new DaoException(e);
+        }
+    }
 
     @Override
     public List<Occupation> findDuplicates(String name) {
+        try {
+            Connection cnn= this.db.getConnection();
+
+            Statement stm = cnn.createStatement ();
+
+            // Result set get the result of the SQL query
+            ResultSet resultSet = stm.executeQuery ("SELECT * from \"OCCUPATION\"");
+
+            // Build the occupation list.
+            List<Occupation> list = new ArrayList<>();
+            while (resultSet.next()) {
+                String res_name= resultSet.getString("NAME");
+                if (name.equals(res_name)) {
+                    int id= resultSet.getInt("ID");
+                    String description = resultSet.getString("DESCRIPTION");
+
+                    Occupation oc = new Occupation(String.valueOf(id),res_name,description);
+
+                    list.add (oc);
+                }
+            }
+
+            // Return the occupations.
+            return list;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public void add(Occupation occupation) {
+        try {
+            Connection cnn= this.db.getConnection();
+
+            cnn.createStatement ().
+                    executeUpdate ("INSERT INTO \"COUNTRY\" (\"NAME\",\"DESCRIPTION\") VALUES ('"+occupation.getName()+"','"+occupation.getDescription()+"')");
+
+        } catch (Exception e) {
+
+            e.printStackTrace ();
+        }
 
     }
 
     @Override
-    public Occupation searchById(String id) {
+    public Occupation searchById(String id)  {
+        try {
+
+            Connection cnn= this.db.getConnection();
+
+            Statement stm = cnn.createStatement ();
+
+            // Result set get the result of the SQL query
+            ResultSet resultSet = stm.executeQuery ("SELECT * from \"OCCUPATION\" WHERE ID="+id);
+
+            if(!resultSet.isBeforeFirst()){
+                int res_id= resultSet.getInt("ID");
+                String name= resultSet.getString("NAME");
+                String description = resultSet.getString("DESCRIPTION");
+
+                Occupation oc = new Occupation(String.valueOf(res_id),name,description);
+                return oc;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Occupation searchByName(String name) {
+        try {
+
+            Connection cnn= this.db.getConnection();
+
+            Statement stm = cnn.createStatement ();
+
+            // Result set get the result of the SQL query
+            ResultSet resultSet = stm.executeQuery ("SELECT * from \"OCCUPATION\" WHERE NAME="+name);
+
+            if(!resultSet.isBeforeFirst()){
+                int id= resultSet.getInt("ID");
+                String res_name= resultSet.getString("NAME");
+                String description = resultSet.getString("DESCRIPTION");
+
+                Occupation oc = new Occupation(String.valueOf(id),res_name,description);
+                return oc;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
+
     }
 
     @Override
