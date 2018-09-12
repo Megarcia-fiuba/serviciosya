071ac1d404b46f28e82da12ae61377b73f82791a
Matias <megarcia@fi.uba.ar>
2018-09-11 23:03:14 -0300

    OcupationJdbcDao implemented

--- src/main/java/com/cagmeini/serviciosya/dao/OccupationJdbcDao.java
+++ src/main/java/com/cagmeini/serviciosya/dao/OccupationJdbcDao.java
@@ -13,28 +13,27 @@ import java.util.Set;
 
 public class OccupationJdbcDao extends CapgeminiDB implements IOccupationDao   {
 
-    private CapgeminiDB db;
 
     public OccupationJdbcDao() throws Exception {
     }
 
     @Override
-    public List<Occupation> findAllOccupations() throws Exception {
+    public List<Occupation> findAllOccupations() {
         try {
 
-            Connection cnn= this.db.getConnection();
+            Connection cnn= this.getConnection();
 
             Statement stm = cnn.createStatement ();
 
             // Result set get the result of the SQL query
-            ResultSet resultSet = stm.executeQuery ("SELECT * from \"OCCUPATION\"");
+            ResultSet resultSet = stm.executeQuery ("SELECT * FROM \"OCCUPATION\"");
 
             // Build the occupation list.
             List<Occupation> list = new ArrayList<>();
             while (resultSet.next()) {
-                int id= resultSet.getInt("ID");
-                String name= resultSet.getString("NAME");
-                String description = resultSet.getString("DESCRIPTION");
+                int id= resultSet.getInt("\"ID\"");
+                String name= resultSet.getString("\"NAME\"");
+                String description = resultSet.getString("\"DESCRIPTION\"");
 
                 Occupation oc = new Occupation(String.valueOf(id),name,description);
 
@@ -52,21 +51,20 @@ public class OccupationJdbcDao extends CapgeminiDB implements IOccupationDao   {
     @Override
     public List<Occupation> findDuplicates(String name) {
         try {
-            Connection cnn= this.db.getConnection();
+            Connection cnn= this.getConnection();
 
             Statement stm = cnn.createStatement ();
 
             // Result set get the result of the SQL query
-            ResultSet resultSet = stm.executeQuery ("SELECT * from \"OCCUPATION\"");
+            ResultSet resultSet = stm.executeQuery ("SELECT * FROM \"OCCUPATION\"");
 
             // Build the occupation list.
             List<Occupation> list = new ArrayList<>();
             while (resultSet.next()) {
-                String res_name= resultSet.getString("NAME");
+                String res_name= resultSet.getString("\"NAME\"");
                 if (name.equals(res_name)) {
-                    int id= resultSet.getInt("ID");
-                    String description = resultSet.getString("DESCRIPTION");
-
+                    int id= resultSet.getInt("\"ID\"");
+                    String description = resultSet.getString("\"DESCRIPTION\"");
                     Occupation oc = new Occupation(String.valueOf(id),res_name,description);
 
                     list.add (oc);
@@ -85,7 +83,7 @@ public class OccupationJdbcDao extends CapgeminiDB implements IOccupationDao   {
     @Override
     public void add(Occupation occupation) {
         try {
-            Connection cnn= this.db.getConnection();
+            Connection cnn= this.getConnection();
 
             cnn.createStatement ().
                     executeUpdate ("INSERT INTO \"COUNTRY\" (\"NAME\",\"DESCRIPTION\") VALUES ('"+occupation.getName()+"','"+occupation.getDescription()+"')");
@@ -101,20 +99,19 @@ public class OccupationJdbcDao extends CapgeminiDB implements IOccupationDao   {
     public Occupation searchById(String id)  {
         try {
 
-            Connection cnn= this.db.getConnection();
+            Connection cnn= this.getConnection();
 
             Statement stm = cnn.createStatement ();
 
             // Result set get the result of the SQL query
-            ResultSet resultSet = stm.executeQuery ("SELECT * from \"OCCUPATION\" WHERE ID="+id);
+            ResultSet resultSet = stm.executeQuery ("SELECT * FROM \"OCCUPATION\" WHERE \"ID\"="+id);
 
             if(!resultSet.isBeforeFirst()){
-                int res_id= resultSet.getInt("ID");
-                String name= resultSet.getString("NAME");
-                String description = resultSet.getString("DESCRIPTION");
+                int res_id= resultSet.getInt("\"ID\"");
+                String name= resultSet.getString("\"NAME\"");
+                String description = resultSet.getString("\"DESCRIPTION\"");
 
-                Occupation oc = new Occupation(String.valueOf(res_id),name,description);
-                return oc;
+                return new Occupation(String.valueOf(res_id),name,description);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -126,20 +123,19 @@ public class OccupationJdbcDao extends CapgeminiDB implements IOccupationDao   {
     public Occupation searchByName(String name) {
         try {
 
-            Connection cnn= this.db.getConnection();
+            Connection cnn= this.getConnection();
 
             Statement stm = cnn.createStatement ();
 
             // Result set get the result of the SQL query
-            ResultSet resultSet = stm.executeQuery ("SELECT * from \"OCCUPATION\" WHERE NAME="+name);
+            ResultSet resultSet = stm.executeQuery ("SELECT * FROM \"OCCUPATION\" WHERE \"NAME\" ="+name);
 
             if(!resultSet.isBeforeFirst()){
-                int id= resultSet.getInt("ID");
-                String res_name= resultSet.getString("NAME");
-                String description = resultSet.getString("DESCRIPTION");
+                int id= resultSet.getInt("\"ID\"");
+                String res_name= resultSet.getString("\"NAME\"");
+                String description = resultSet.getString("\"DESCRIPTION\"");
 
-                Occupation oc = new Occupation(String.valueOf(id),res_name,description);
-                return oc;
+                return new Occupation(String.valueOf(id),res_name,description);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -150,22 +146,70 @@ public class OccupationJdbcDao extends CapgeminiDB implements IOccupationDao   {
 
     @Override
     public String getDescprition(String id) {
+
+        try {
+
+            Connection cnn= this.getConnection();
+
+            Statement stm = cnn.createStatement ();
+
+            // Result set get the result of the SQL query
+            ResultSet resultSet = stm.executeQuery ("SELECT * FROM \"OCCUPATION\" WHERE \"ID\"="+id);
+
+            if(!resultSet.isBeforeFirst()){
+                return resultSet.getString("\"DESCRIPTION\"");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public boolean exists(String id) {
+        try {
+
+            Connection cnn= this.getConnection();
+
+            Statement stm = cnn.createStatement ();
+
+            // Result set get the result of the SQL query
+            ResultSet resultSet = stm.executeQuery ("SELECT * FROM \"OCCUPATION\" WHERE \"ID\"="+id);
+
+            return !resultSet.isBeforeFirst();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public void modifyName(String id, String newName) {
+        try{
+            Connection cnn= this.getConnection();
 
+            Statement stm = cnn.createStatement ();
+
+            int r = stm.executeUpdate ("UPDATE \"OCCUPATION\" SET \"NAME\" = '"+newName+"' WHERE \"ID\"="+id);
+
+        }catch (Exception e){
+            throw new DaoException(e);
+        }
     }
 
     @Override
     public void modifyDescription(String id, String newDescription) {
+        try{
+            Connection cnn= this.getConnection();
 
+            Statement stm = cnn.createStatement ();
+
+            int r = stm.executeUpdate ("UPDATE \"OCCUPATION\" SET \"DESCRIPTION\" = '"+newDescription+"' WHERE \"ID\"="+id);
+
+
+        }catch (Exception e){
+            throw new DaoException(e);
+        }
     }
 
     @Override
@@ -175,11 +219,31 @@ public class OccupationJdbcDao extends CapgeminiDB implements IOccupationDao   {
 
     @Override
     public void remove(String id) {
+        try{
+            Connection cnn= this.getConnection();
+
+            Statement stm = cnn.createStatement ();
+
+            int r = stm.executeUpdate ("DELETE FROM \"OCCUPATION\"  WHERE \"ID\"="+id);
+        }catch (Exception e){
+            throw new DaoException(e);
+        }
+
 
     }
 
     @Override
     public void removeAll() {
+        try{
+            Connection cnn= this.getConnection();
+
+            Statement stm = cnn.createStatement ();
+
+            int r = stm.executeUpdate ("DELETE FROM \"OCCUPATION\"");
+        }catch (Exception e){
+            throw new DaoException(e);
+        }
+
 
     }
 }
