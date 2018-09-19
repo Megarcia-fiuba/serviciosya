958cd6cc81b1366d269ecddd10efcfead539ca6b
Megarcia-fiuba <megarcia@fi.uba.ar>
2018-09-19 09:06:41 -0300

    Added spring boot

--- api/src/main/java/com/capgemini/serviciosya/dao/ICountryDao.java
+++ api/src/main/java/com/capgemini/serviciosya/dao/ICountryDao.java
@@ -1,6 +1,9 @@
 package com.capgemini.serviciosya.dao;
 
 import com.capgemini.serviciosya.beans.entity.CountryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ICountryDao extends IDao<CountryEntity, Integer> {
+@Repository
+public interface ICountryDao extends JpaRepository<CountryEntity, Integer> {
 }
